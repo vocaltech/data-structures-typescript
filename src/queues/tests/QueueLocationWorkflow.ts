20019ebaba61142b96bb6df54ea15a7fe61e136f
vocaltech <it@vocaltech.fr>
2022-01-16 15:02:30 +0100

    Initial commit

---
+++ src/queues/tests/QueueLocationWorkflow.ts
@@ -0,0 +1,47 @@
+import { Queue } from "../Queue";
+import { Location } from './models/Location';
+
+let queueLocation = new Queue<Location>();
+
+const displayPeekAndQueue = (queue: Queue<Location>) => {
+    let p = queue.peek();
+    console.log(`peek: ` + JSON.stringify(p, null, 2));
+    
+    console.log(queue);
+    console.log("\n")
+}
+
+const pause = (ms: number): Promise<void> => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+const workkflow = async () => {
+    let location: Location = new Location(45.3, 1.5, new Date());
+    queueLocation.enqueue(location);
+    displayPeekAndQueue(queueLocation)
+
+    await pause(2000);
+
+    location = new Location(48.1, 1.8, new Date());
+    queueLocation.enqueue(location);
+    displayPeekAndQueue(queueLocation);
+
+    await pause(2000);
+
+    location = new Location(48.4, 1.9, new Date());
+    queueLocation.enqueue(location);
+    displayPeekAndQueue(queueLocation);
+
+    await pause(2000);
+
+    queueLocation.dequeue();
+    console.log(`------- after dequeue --------\n`);
+    displayPeekAndQueue(queueLocation);
+
+}
+
+workkflow();
+
+
