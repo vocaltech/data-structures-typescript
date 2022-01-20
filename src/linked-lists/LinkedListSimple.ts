05b957885f7e65984c43967750d5039f28b51c2a
vocaltech <it@vocaltech.fr>
2022-01-20 23:56:53 +0100

    insertFirst implementation

--- src/linked-lists/LinkedListSimple.ts
+++ src/linked-lists/LinkedListSimple.ts
@@ -1,8 +1,40 @@
 import { ILinkedList } from "./ILinkedList";
+import { Node } from "./Node";
 
 export class LinkedListSimple<T> implements ILinkedList<T> {
-    insertFirst(item: T) {
+    private _head: Node<T>
+    private _tail: Node<T>
+
+    constructor() {
+        this._head = new Node<T>(null)
+        this._tail = new Node<T>(null)
+        this._head.next = this._tail
+    }
+
+    // setters and getters
+    get head() {
+        return this._head
+    }
+
+    get tail() {
+        return this._tail
+    }
+
+
+    isEmpty(): boolean {
         throw new Error('Not yet implemented')
+    }
+
+    getFirst(): T | null {
+        throw new Error('Not yet implemented')
+    }
+
+    insertFirst(item: T) {
+        const newNode = new Node(item)
+
+        newNode.next = this._head.next
+        this._head.next = newNode
+        
     }
 
     insertLast(item: T) {
@@ -20,13 +52,4 @@ export class LinkedListSimple<T> implements ILinkedList<T> {
     contains(searchItem: T): boolean {
         throw new Error('Not yet implemented')
     }
-
-    isEmpty(): boolean {
-        throw new Error('Not yet implemented')
-    }
-
-    getFirst(): T | null {
-        throw new Error('Not yet implemented')
-    }
-
 }
