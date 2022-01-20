d398693fa102418041dce31520976e68c0da424c
vocaltech <it@vocaltech.fr>
2022-01-20 23:55:09 +0100

    update with getters/setters

--- src/linked-lists/Node.ts
+++ src/linked-lists/Node.ts
@@ -1,9 +1,21 @@
 export class Node<T> {
-    private next: Node<T> | null;
-    private item: T | null;
+    private _next: Node<T> | null;
+    private _item: T | null;
 
     constructor(item: T | null = null) {
-        this.item = item;
-        this.next = null;
+        this._item = item;
+        this._next = null;
+    }
+
+    get next(): Node<T> | null {
+        return this._next;
+    }
+
+    set next(node: Node<T> | null) {
+        this._next = node;
+    }
+
+    get item() {
+        return this._item
     }
 }
