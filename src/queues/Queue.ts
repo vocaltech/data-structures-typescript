bd43229190b764f0852804abded730e174dc1510
vocaltech <it@vocaltech.fr>
2022-01-27 23:22:54 +0100

    constructor: add maxSize param / refactor enqueue

--- src/queues/Queue.ts
+++ src/queues/Queue.ts
@@ -2,21 +2,49 @@ import { IQueue } from "./IQueue";
 
 export class Queue<T> implements IQueue<T> {
     private queue: T[];
+    private maxSize: number = 0;
+    private enqueueIdx: number = 0;
 
-    constructor(...args: T[]) {
-        this.queue = [...args];
+    constructor(maxSize?: number, ...args: T[]) {
+        if (maxSize) { // maxSize is defined
+            this.queue = new Array<T>(maxSize);
+            this.maxSize = maxSize;
+        } else {
+            this.queue = [...args];
+        }
     }
 
     contents(): T[] {
         return this.queue
+    }
+
+    getMaxSize(): number {
+        return this.maxSize;
     }
     
     size(): number {
         return this.queue.length;
     }
 
+    // TODO: enqueue()
     enqueue(...args: T[]): number {
-        return this.queue.push(...args);
+        if (this.maxSize === 0) { // no maxSize defined
+            return this.queue.push(...args);
+        } else {
+            let curIdx = 0;
+            for (const elt of this.queue) {
+                if (curIdx === args.length) {
+                    break;
+                } else if (elt === undefined) {
+                    console.log(`undefined / curIdx: ${curIdx} / enqueueIdx: ${this.enqueueIdx} / args: ${args}`);
+                    this.queue[this.enqueueIdx] = args[curIdx];
+                    curIdx++;;
+
+                    this.enqueueIdx++;
+                }
+            }
+            return 0;
+        }
     }
 
     dequeue(): T {
@@ -31,8 +59,7 @@ export class Queue<T> implements IQueue<T> {
         return (this.queue.length === 0 ? true: false)
     }
 
-    // TODO: isFull()
     isFull(): boolean  {
-        return false;
+        return this.queue.length === this.maxSize
     }
 }
