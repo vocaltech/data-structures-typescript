a865b81e7b9de8e8a7b253c3d415c4b9f0e5ee21
vocaltech <it@vocaltech.fr>
2022-01-29 00:43:53 +0100

    Added linked-list

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './queues/IQueue';
 export * from './queues/Queue';
 export * from './stacks/IStack';
 export * from './stacks/Stack';
+export * from './linked-lists/ILinkedList';
+export * from './linked-lists/LinkedListSimple';
