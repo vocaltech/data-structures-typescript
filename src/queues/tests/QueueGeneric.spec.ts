32cc2079a8c9bd0ef76ac9d6059cb5857c07b37b
vocaltech <it@vocaltech.fr>
2022-01-31 22:05:35 +0100

    split tests with number into multiple sub-describe

--- src/queues/tests/QueueGeneric.spec.ts
+++ src/queues/tests/QueueGeneric.spec.ts
@@ -1,81 +1,151 @@
 import { Queue } from '../Queue';
 import { Location } from '../../common/models/Location'
 
-describe('tests numbers - with initial capacity ', () => {
+describe('tests with numbers - initial capacity ', () => {
     let queue: Queue<number>;
 
-    it(' should construct a queue with initial capacity', () => {
-      queue = new Queue<number>(4);
+    describe('constructor with some checks ', () => {
+        it(' should construct a queue with initial capacity', () => {
+            queue = new Queue<number>(4);
+      
+            const capacity = queue.getCapacity();
+            expect(capacity).toBe(4)
+      
+            const size = queue.size()
+            expect(size).toBe(0)
+        })
 
-      const capacity = queue.getCapacity();
-      expect(capacity).toBe(4)
+        it(' isFull should be false', () => {
+            expect(queue.isFull()).toBeFalsy()
+        });
 
-      const size = queue.size()
-      expect(size).toBe(0)
-    })
-
-    it (' should dequeue after init', () => {
-        const isEmpty = queue.isEmpty();
-        expect(isEmpty).toBeTruthy()
-
-        let dq = queue.dequeue();
-        expect(dq).toBeNull()
-
-        console.log('--- should dequeue after init ---');
-        console.log(queue.contents());
-    })
-
-    it(' should peek after init', () => {
-        expect(queue.peek()).toBeNull();
+        it( ' isEmpty should be true', () => {
+            expect(queue.isEmpty()).toBeTruthy()
+        });
+        
+        it (' dequeue after init should return null', () => {    
+            let dq = queue.dequeue();
+            expect(dq).toBeNull()
+    
+            console.log('--- should dequeue after init ---');
+            console.log(queue.contents());
+        })
+    
+        it(' peek after init should return null', () => {
+            expect(queue.peek()).toBeNull();
+        });
     });
     
+    describe('enqueue tests', () => {
+        it(' should enqueue(9, 3)', () => {
+            let queueResult = queue.enqueue(9, 3);
+    
+            expect(queue.isEmpty()).toBeFalsy()
+            expect(queue.isFull()).toBeFalsy();
+    
+            expect(queueResult).toBe(2);
+            expect(queue.size()).toBe(2)
+    
+            expect(queue.peek()).toBe(9)
 
-    it(' should enqueue(9, 3)', () => {
-        let queueRes = queue.enqueue(9, 3);
+            expect(queue.contents()).toEqual(expect.arrayContaining([9, 3]))
+            expect(queue.contents()).toEqual([9, 3, undefined, undefined])
 
-        expect(queue.isEmpty()).toBeFalsy()
-        expect(queue.isFull()).toBeFalsy();
+            console.log('--- should enqueue(9,3) --- ');
+            console.log(queue.contents());
+        });
 
-        expect(queueRes).toBe(2);
-        expect(queue.size()).toBe(2)
+        it(' should enqueue(2)', () => {
+            let queueResult = queue.enqueue(2);
 
-        expect(queue.peek()).toBe(9)
+            expect(queue.isEmpty()).toBeFalsy()
+            expect(queue.isFull()).toBeFalsy();
+    
+            expect(queueResult).toBe(3);
+            expect(queue.size()).toBe(3)
+    
+            expect(queue.peek()).toBe(9)
 
-        console.log('--- should enqueue(9,3) --- ');
-        console.log(queue.contents());
+            expect(queue.contents()).toEqual(expect.arrayContaining([9, 3, 2]))
+            expect(queue.contents()).toEqual([9, 3, 2, undefined])
+    
+            console.log('--- should enqueue(2) --- ');
+            console.log(queue.contents());
+        });
+
+        it(' should enqueue(5, 7, 8, 1)', () => {
+            let queueResult = queue.enqueue(5, 7, 8, 1);
+
+            expect(queue.isEmpty()).toBeFalsy()
+            expect(queue.isFull()).toBeTruthy();
+    
+            expect(queueResult).toBe(4);
+            expect(queue.size()).toBe(4)
+    
+            expect(queue.peek()).toBe(9)
+
+            expect(queue.contents()).toEqual(expect.arrayContaining([9, 3, 2, 5]))
+            expect(queue.contents()).toEqual([9, 3, 2, 5])
+    
+            console.log('--- should enqueue(5, 7, 8, 1) --- ');
+            console.log(queue.contents());
+        });
     });
+
+    describe('dequeue tests', () => {
+        it(' should dequeue 1', () => {
+            // [9, 3, 2, 5]
+            expect(queue.peek()).toBe(9)
+            expect(queue.dequeue()).toBe(9)
+            expect(queue.contents()).toEqual([3, 2, 5, undefined])
+
+            console.log('--- dequeue 1 ---');
+            console.log(queue.contents());
+        });
+
+        it(' should dequeue 2', () => {
+            // [3, 2, 5, undefined]
+            expect(queue.peek()).toBe(3)
+            expect(queue.dequeue()).toBe(3)
+            expect(queue.contents()).toEqual([2, 5, undefined, undefined])
+
+            console.log('--- dequeue 2 ---');
+            console.log(queue.contents());
+        });
+
+        it(' should dequeue 3', () => {
+            // [2, 5, undefined, undefined]
+            expect(queue.peek()).toBe(2)
+            expect(queue.dequeue()).toBe(2)
+            expect(queue.contents()).toEqual([5, undefined, undefined, undefined])
+
+            console.log('--- dequeue 3 ---');
+            console.log(queue.contents());
+        });
+
+        it(' should dequeue 4', () => {
+            // [5, undefined, undefined, undefined]
+            expect(queue.peek()).toBe(5)
+            expect(queue.dequeue()).toBe(5)
+            expect(queue.contents()).toEqual([undefined, undefined, undefined, undefined])
+            expect(queue.isEmpty()).toBeTruthy();
+
+            console.log('--- dequeue 4 ---');
+            console.log(queue.contents());
+        });
+
+        it(' should dequeue empty and return null', () => {
+            expect(queue.peek()).toBeNull();
+            expect(queue.dequeue()).toBeNull()
+        });
+    });
+
+    describe('tests with enqueue/dequeue', () => {
+        // TODO: tests with enqueue/dequeue
+    });
+    
     
 /*
-      queue.enqueue(2);
-      expect(queue.isFull()).toBeFalsy();
-      console.log(queue.contents());
-
-      queue.enqueue(5, 7);
-      console.log(queue.contents());
-      expect(queue.isFull()).toBeTruthy()
-
-      queue.enqueue(8, 6);
-      expect(queue.isFull()).toBeTruthy();
-      console.log(queue.contents());
-
-      let pk = queue.peek()
-      console.log(`peek: ${pk}`);
-
-      console.log('#### dequeue ####');
-      dq = queue.dequeue();
-      console.log(`dq: ${dq}`);
-      console.log(queue.contents());
-
-      console.log('#### dequeue ####');
-      dq = queue.dequeue();
-      console.log(`dq: ${dq}`);
-      console.log(queue.contents());
-
-      console.log('#### dequeue ####');
-      dq = queue.dequeue();
-      console.log(`dq: ${dq}`);
-      console.log(queue.contents());
-
       console.log('### enqueue(9) ###');
       queue.enqueue(9);
       console.log(queue.contents());
