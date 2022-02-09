aa34f0b5afcdc7e7da591056b922137c915a0f44
vocaltech <it@vocaltech.fr>
2022-02-09 23:49:19 +0100

    update

--- src/queues/debug/QueueDebug.ts
+++ src/queues/debug/QueueDebug.ts
@@ -1,13 +1,17 @@
 import { Queue } from '../Queue'
 
 let queue = new Queue<number>(4);
-let enqueueResult = queue.enqueue(9)
+
+let isEmpty = queue.isEmpty() // should be true
+console.log(isEmpty)
+
+let enqueueResult = queue.enqueue(9, 3)
 console.log(enqueueResult);
 
-enqueueResult = queue.enqueue(8, 6);
-console.log(enqueueResult);
+let queueContents = queue.contents() // bug: returns []
+console.log(queueContents) 
 
-let peek = queue.peek() // 9
-let dequeueResult = queue.dequeue() // 9
-let sizeAfterDequeue = queue.size() // 2
-console.log(sizeAfterDequeue);
+isEmpty = queue.isEmpty() // should be false
+
+
+
