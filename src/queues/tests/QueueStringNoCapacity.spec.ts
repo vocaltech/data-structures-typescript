e69c257e4987b421695f52421f7a97f46446e8cc
vocaltech <it@vocaltech.fr>
2022-02-10 23:24:50 +0100

    initial commit

---
+++ src/queues/tests/QueueStringNoCapacity.spec.ts
@@ -0,0 +1,15 @@
+// TODO: tests with strings
+/*
+describe.skip('tests with strings', () => {
+    it('should enqueue 1 string', () => {
+        let queue = new Queue<string>("v", "o")
+
+        expect(queue.size()).toBe(2)
+        expect(typeof queue.peek()).toBe("string")
+
+        queue.enqueue("c", "a");
+        expect(queue.size()).toBe(4)
+        expect(queue.peek()).toBe("v")
+    })
+})
+*/
