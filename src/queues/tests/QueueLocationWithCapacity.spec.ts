5e6d0c61530648ad6a9a143e984e852a132c90a2
vocaltech <it@vocaltech.fr>
2022-02-11 22:54:08 +0100

    Initial commit

---
+++ src/queues/tests/QueueLocationWithCapacity.spec.ts
@@ -0,0 +1,25 @@
+import { Queue } from '../Queue'
+import { Location } from '../../common/models/Location'
+
+describe('tests with locations (initial capacity)', () => {
+    let queue: Queue<Location>;
+
+    // TODO: constructor with initial capacity, without params
+    describe('constructor with initial capacity, without params', () => {})
+
+    // TODO: constructor with initial capacity, with params
+    describe('constructor with initial capacity, with params', () => {})
+
+    // TODO: enqueue tests
+    describe('enqueue tests', () => {})
+
+    // TODO: dequeue tests
+    describe('dequeue tests', () => {})
+
+    // TODO: tests with enqueue/dequeue mixins (without params)
+    describe('tests with enqueue/dequeue mixins (without params)', () => {})
+
+    // TODO: tests with enqueue/dequeue mixins (with params)
+    describe('tests with enqueue/dequeue mixins (with params)', () => {})
+
+})
